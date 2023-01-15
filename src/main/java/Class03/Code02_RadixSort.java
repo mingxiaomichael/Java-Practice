7ef58d6ef6cd02a60c6ee8e7508e4bf48294044e
MingxiaoLi <lmx@umich.edu>
2023-01-15 01:33:30 -0500

    Radix improved

--- src/main/java/Class03/Code02_RadixSort.java
+++ src/main/java/Class03/Code02_RadixSort.java
@@ -1,114 +1,59 @@
-//package Class03;
-//
-///*
-//    RadixSort
-// */
-//
-//public class Code02_RadixSort {
-//    public static void radixSort(int[] arr){
-//        if(arr == null || arr.length < 2){
-//            return;
-//        }
-//        radixSort(arr, 0, arr.length - 1, maxBits(arr));
-//    }
-//
-//    public static int maxBits(int[] arr){
-//        int max = Integer.MIN_VALUE;
-//        for(int i = 0; i < arr.length; i++){
-//            max = Math.max(max, arr[i]);
-//        }
-//        int res = 0;
-//        while(max != 0){
-//            res++;
-//            max /= 10;
-//        }
-//        return res;
-//    }
-//
-//    public static void radixSort(int[] arr, int begin, int end, int digit){
-//        final int radix = 10;
-//        int i = 0, j = 0;
-//        int[] bucket = new int[end - begin + 1];
-//        for(int d = 1; d <= digit; d++){
-//            int[] count = new int[radix];
-//            for(i = begin; i <= end; i++){
-//                j = getDigit(arr[i], d);
-//                count[j]++;
-//            }
-//            for(i = 1; i < radix; i++){
-//                count[i] = count[i] + count[i - 1];
-//            }
-//            for(i = end; i >= begin; i--){
-//                j = getDigit(arr[i], d);
-//                bucket[count[j] - 1] = arr[i];
-//                count[j]--;
-//            }
-//            for(i = begin, j = 0; i <= end; i++, j--){
-//                arr[i] = bucket[j];
-//            }
-//        }
-//    }
-//
-//    public static int getDigit(int x, int d){
-//        return ((x / ((int)Math.pow(10, d - 1))) % 10);
-//    }
-//
-//}
-
 package Class03;
 
-import java.util.Arrays;
+/*
+    RadixSort
+    Time Complexity: O(n + k)
+    Space Complexity: O(n + k)
+    Limited by data status
+ */
 
 public class Code02_RadixSort {
-
-    // only for no-negative value
-    public static void radixSort(int[] arr) {
-        if (arr == null || arr.length < 2) {
+    public static void radixSort(int[] arr){
+        if(arr == null || arr.length < 2){
             return;
         }
-        radixSort(arr, 0, arr.length - 1, maxbits(arr));
+        radixSort(arr, 0, arr.length - 1, maxBits(arr));
     }
 
-    public static int maxbits(int[] arr) {
+    public static int maxBits(int[] arr){
         int max = Integer.MIN_VALUE;
-        for (int i = 0; i < arr.length; i++) {
+        for(int i = 0; i < arr.length; i++){
             max = Math.max(max, arr[i]);
         }
         int res = 0;
-        while (max != 0) {
+        while(max != 0){
             res++;
             max /= 10;
         }
         return res;
     }
 
-    public static void radixSort(int[] arr, int begin, int end, int digit) {
+    public static void radixSort(int[] arr, int begin, int end, int digit){
         final int radix = 10;
         int i = 0, j = 0;
-
         int[] bucket = new int[end - begin + 1];
-        for (int d = 1; d <= digit; d++) {
+        for(int d = 1; d <= digit; d++){
             int[] count = new int[radix];
-            for (i = begin; i <= end; i++) {
+            for(i = begin; i <= end; i++){
                 j = getDigit(arr[i], d);
                 count[j]++;
             }
-            for (i = 1; i < radix; i++) {
+            for(i = 1; i < radix; i++){
                 count[i] = count[i] + count[i - 1];
             }
-            for (i = end; i >= begin; i--) {
+            for(i = end; i >= begin; i--){
                 j = getDigit(arr[i], d);
                 bucket[count[j] - 1] = arr[i];
                 count[j]--;
             }
-            for (i = begin, j = 0; i <= end; i++, j++) {
+            for(i = begin, j = 0; i <= end; i++, j++){
                 arr[i] = bucket[j];
             }
         }
     }
 
-    public static int getDigit(int x, int d) {
-        return ((x / ((int) Math.pow(10, d - 1))) % 10);
+    public static int getDigit(int x, int d){
+        return ((x / ((int)Math.pow(10, d - 1))) % 10);
     }
 
 }
